156a38ae17eadd26bcf5472fca9be9b5ffc0e423
Jay <1598353326@qq.com>
2022-06-04 21:53:51 +0800

    feat: toast功能实现
    toast支持success、warn、error、promise等类型
    支持全局配置
    支持多位置、暂停、倒序、手动关闭、SSR
    支持自定义挂载容器、渲染容器、icon
    提供hooks获取状态

--- .cz-config.js
+++ .cz-config.js
@@ -1,18 +1,22 @@
 module.exports = {
   types: [
-    { value: '✨ feat', name: '✨ feat:       A new feature' },
-    { value: '🔧 fix', name: '🔧 fix:        A bug fix' },
-    { value: '📝 docs', name: '📝 docs:       Documentation only changes' },
-    { value: '🎨 style', name: '🎨 style:      Changes that do not affect the meaning of the code' },
-    { value: '♻️ refactor', name: '♻️  refactor:   A code change that neither fixes a bug nor adds a feature' },
-    { value: '🚀 perf', name: '🚀 perf:       A code change that improves performance' },
-    { value: '🧪 test', name: '🧪 test:       Adding missing or correcting existing tests' },
-    { value: '👷 ci', name: '👷 ci:         CI configuration related e.g. changes to k8s，docker configuration files' },
-    { value: '📦‍ build', name: '📦‍ build:      Changes to the build process or auxiliary tools' },
-    { value: '🍻 chore', name: '🍻 chore:      chroe' },
-    { value: '💊 revert', name: '💊 revert:     Reverts a previous commi' },
+    { value: 'feat', name: '✨ feat:       A new feature' },
+    { value: 'fix', name: '🔧 fix:        A bug fix' },
+    { value: 'docs', name: '📝 docs:       Documentation only changes' },
+    { value: 'style', name: '🎨 style:      Changes that do not affect the meaning of the code' },
+    { value: 'refactor', name: '♻️  refactor:   A code change that neither fixes a bug nor adds a feature' },
+    { value: 'perf', name: '🚀 perf:       A code change that improves performance' },
+    { value: 'test', name: '🧪 test:       Adding missing or correcting existing tests' },
+    { value: 'ci', name: '👷 ci:         CI configuration related e.g. changes to k8s，docker configuration files' },
+    { value: 'build', name: '📦‍ build:      Changes to the build process or auxiliary tools' },
+    { value: 'chore', name: '🍻 chore:      chroe' },
+    { value: 'revert', name: '💊 revert:     Reverts a previous commit' }
   ],
-  scopes: [{ name: 'docs' }, { name: 'ci' }, { name: 'style' }],
+  scopes: [
+    { name: 'docs' },
+    { name: 'ci' },
+    { name: 'style' },
+  ],
 
   // allowTicketNumber: false,
   // isTicketNumberRequired: false,
@@ -50,4 +54,4 @@ module.exports = {
   // breaklineChar: '|', // It is supported for fields body and footer.
   // footerPrefix : 'ISSUES CLOSED:'
   // askForBreakingChangeFirst : true, // default is false
-};
+}
